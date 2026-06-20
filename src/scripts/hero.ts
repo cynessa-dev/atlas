dbf33928f101f9d8466bc9d1e731bcee15492732
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-06-20 14:49:05 +0800

    ✨ feat: implement boot messages display and animation in Hero component

--- src/scripts/hero.ts
+++ src/scripts/hero.ts
@@ -9,6 +9,7 @@ export const initHero = () => {
     if (!propsContainer) return; // Prop container is essential to animate, otherwise exit
 
     playEntranceAnimation();
+    playBootMessages();
 };
 
 const playEntranceAnimation = () => {
@@ -17,5 +18,19 @@ const playEntranceAnimation = () => {
         scale: 1.05,
         duration: 5,
         ease: 'sine.out',
+    });
+};
+
+const playBootMessages = () => {
+    const timeline = gsap.timeline();
+    const messages = document.querySelectorAll('[data-boot-message]');
+
+    if (!messages) return;
+
+    messages.forEach((message, index) => {
+        timeline.from(message, {
+            autoAlpha: 0,
+            duration: 0.25,
+        });
     });
 };
