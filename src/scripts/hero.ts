e947f0d9187d8a4a8179a05ab0891b933ec6764b
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-06-20 11:01:29 +0800

    🎨 refactor: add comments for documentation

--- src/scripts/hero.ts
+++ src/scripts/hero.ts
@@ -3,14 +3,16 @@ import gsap from 'gsap';
 let propsContainer: HTMLElement | null = null
 
 export const initHero = () => {
+    // Get the prop container instead of each children to scale evenly
     propsContainer = document.getElementById('props');
 
-    if (!propsContainer) return;
+    if (!propsContainer) return; // Prop container is essential to animate, otherwise exit
 
     playEntranceAnimation();
 };
 
 const playEntranceAnimation = () => {
+    // Subtle zoom in to give importance to the subject
     gsap.to(propsContainer, {
         scale: 1.05,
         duration: 5,
