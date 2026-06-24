7bb9551bf9de377307eb8f13773c3059222501e1
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-06-24 16:10:45 +0800

    ✨ feat: enhance MenuIcon functionality with menu toggle animation

--- src/scripts/header.ts
+++ src/scripts/header.ts
@@ -1,7 +1,36 @@
 import gsap from 'gsap';
 
+type MenuLine = 'top' | 'mid' | 'bot';
+
+const menuTimeline = gsap.timeline({ paused: true });
+
+let menuButton: HTMLButtonElement | null = null
+let icon: HTMLElement | null = null
+
 export const initHeader = () => {
+    menuButton = document.querySelector('[data-menu-button]');
+    icon = document.querySelector('[data-menu-icon]');
+
+    if (!menuButton || !icon) return;
+
+    menuButton.addEventListener('click', handleMenuClick)
+
+    createMenuAnimation();
     playEntranceAnimation();
+};
+
+const handleMenuClick = () => {
+    renderMenuIcon();
+};
+
+// ====================
+// ANIMATIONS
+// ====================
+
+const renderMenuIcon = () => {
+    const isOpen = toggleMenu();
+
+    isOpen ? menuTimeline.play() : menuTimeline.reverse();
 };
 
 const playEntranceAnimation = () => {
@@ -26,4 +55,57 @@ const playEntranceAnimation = () => {
             duration: 0.3,
             ease: 'sine.out'
         });
+};
+
+const createMenuAnimation = () => {
+    const lines = {
+        top: getLine('top'),
+        mid: getLine('mid'),
+        bot: getLine('bot'),
+    };
+
+    if (!lines.top || !lines.mid || !lines.bot) return;
+
+    const topY = Number(lines.top.getAttribute('y'));
+    const midY = Number(lines.mid.getAttribute('y'));
+    const botY = Number(lines.bot.getAttribute('y'));
+
+    const topMoveY = midY - topY;
+    const botMoveY = midY - botY;
+
+    const duration = 0.3;
+    const ease = 'sine.in';
+
+    menuTimeline
+        .to(lines.top, {
+            y: topMoveY,
+            duration: duration,
+            ease: ease,
+        }, 0)
+        .to(lines.bot, {
+            y: botMoveY,
+            duration: duration,
+            ease: ease,
+        }, 0);
+};
+
+// ====================
+// UTILITY FUNCTIONS
+// ====================
+
+const getLine = (line: MenuLine) => {
+    if (!icon) return;
+    
+    return icon.querySelector(`[data-menu-line=${line}]`) ?? null;
 }
+
+const toggleMenu = () => {
+    if (!menuButton) return;
+
+    const isOpen = menuButton.getAttribute('aria-expanded') === 'true';
+    const nextState = !isOpen;
+
+    menuButton.setAttribute('aria-expanded', String(nextState));
+
+    return nextState;
+};
