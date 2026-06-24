3194dae60918d9e2c10c5d00c4400f5259880159
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-06-24 17:00:33 +0800

    ✨ feat: implement toggle overlay functionality for UI enhancement

---
+++ src/scripts/overlay.ts
@@ -0,0 +1,15 @@
+const OVERLAY_OPEN_CLASSES = ['bg-zinc-800/40', 'backdrop-blur-md'];
+
+export const toggleOverlay = (isOpen?: boolean) => {
+    const overlay = getOverlay();
+
+    if (!overlay) return;
+
+    const nextState = isOpen ?? !overlay.classList.contains(OVERLAY_OPEN_CLASSES[1]);
+
+    OVERLAY_OPEN_CLASSES.forEach((className) => {
+        overlay.classList.toggle(className, nextState);
+    });
+}
+
+const getOverlay = () => document.getElementById('overlay');
