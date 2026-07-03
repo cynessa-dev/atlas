15fdb65c0134b9fb3d39e38ce96c182b927cbb1e
Christian Mamplata <261211617+cynessa-dev@users.noreply.github.com>
2026-07-03 18:51:09 +0800

    ✨ feat: refactor animateScramble and scrambleText functions for improved text animation flow

--- src/scripts/scramble.ts
+++ src/scripts/scramble.ts
@@ -3,53 +3,34 @@ import gsap from 'gsap';
 export const animateScramble = (textElement: HTMLElement) => {
     if (!textElement) return;
 
-    const originalText = textElement.textContent;
-
-    scrambleText(textElement);
-    unscrambleText(textElement, originalText);
-}; 
-
-const scrambleText = (textElement: HTMLElement) => {
-    const symbols = '!<>\\/[]{}—=+*^?#';
-    const characters = textElement.textContent.split('') || [];
     const timeline = gsap.timeline();
 
-    // Clear the text content of the element
-    textElement.textContent = '';
+    scrambleText(textElement, timeline);
+    unscrambleText();
+}; 
 
-    // Scramble the text first
-    characters.forEach((char) => {
-        // Whitespaces are always underscore to make the effect look natural, yet random
-        if (char === ' ') {
-            textElement.textContent += '_';
-            return;
-        }
+const scrambleText = (textElement: HTMLElement, timeline: gsap.core.Timeline) => {
+    const symbols = '!<>\\/[]{}()=+*^?#';
+    const characters = (textElement.dataset.scramble ?? '').split('');
 
-        // Get a random symbol, if not a whitespace
-        const randomInt = getRandomInt(0, characters.length - 1);
-        const symbol = symbols.charAt(randomInt);
+    characters.forEach((char, index) => {
+        let symbol = '_'; // Default symbol for whitespaces
+
+        if (char !== ' ') symbol = symbols[getRandomInt(0, symbols.length - 1)];
 
         timeline.to({}, {
-            duration: 0.2,
-            onUpdate: () => textElement.textContent += symbol,
+            duration: 0.02,
+            onStart: () => {
+                characters[index] = symbol;
+
+                textElement.textContent += characters[index];
+            },
         });
     });
 };
 
-const unscrambleText = (textElement: HTMLElement, originalText: string) => {
-    const characters = textElement.textContent.split('') || [];
-    const timeline = gsap.timeline();
-
-    characters.forEach((_, index) => {
-        timeline.to({}, {
-            duration: 0.2,
-            onUpdate: () => {
-                characters[index] = originalText[index];
-
-                textElement.textContent = characters.toString();
-            },
-        });
-    });
+const unscrambleText = () => {
+    
 };
 
 const getRandomInt = (min: number, max: number) => {
